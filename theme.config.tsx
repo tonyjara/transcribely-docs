28e3b22e009fece415fd95b266c9e24cb4fdbbee
Tony Jara <340studiospy@gmail.com>
2023-11-07 12:25:25 -0300

    Updated app name

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>Transcribely Docs</span>,
+  /* project: { */
+  /*   link: 'https://github.com/shuding/nextra-docs-template', */
+  /* }, */
+  /* chat: { */
+  /*   link: "https://discord.com", */
+  /* }, */
+  /* docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template', */
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <div style={{ display: "flex", gap: "20px" }}>
+        <span>Documentation for Transcribely</span>
+        <span>
+          <a
+            style={{ fontWeight: "bold" }}
+            href="https://shipvoid.com"
+            target="_blank"
+          >
+            Buy this starter
+          </a>
+          .
+        </span>
+      </div>
+    ),
   },
-}
+};
 
-export default config
+export default config;
